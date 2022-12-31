0e04b62e5f082c243e7e3c1d7320d2976c3c7164
Sebastiaan <45851377+Kyzegs@users.noreply.github.com>
2022-12-31 18:30:46 +0100

    Ensure endpoints use https

--- pyrez/enumerations/Endpoint.py
+++ pyrez/enumerations/Endpoint.py
@@ -1,14 +1,14 @@
 from .Enum import Enum
 class Endpoint(Enum):
     """Representing an endpoint that you want to access to retrieve information from."""
-    PALADINS = 'http://api.paladins.com/paladinsapi.svc'
-    REALM_ROYALE = 'http://api.realmroyale.com/realmapi.svc'
-    SMITE = 'http://api.smitegame.com/smiteapi.svc'
+    PALADINS = 'https://api.paladins.com/paladinsapi.svc'
+    REALM_ROYALE = 'https://api.realmroyale.com/realmapi.svc'
+    SMITE = 'https://api.smitegame.com/smiteapi.svc'
     HIREZ = 'https://api.hirezstudios.com'
     STATUS_PAGE = 'https://stk4xr7r1y0r.statuspage.io' #http://status.hirezstudios.com
 
-    HAND_OF_THE_GODS = 'http://api.handofthegods.com/handofthegodsapi.svc'
-    PALADINS_STRIKE = 'http://api.paladinsstrike.com/paladinsstrike.svc'
+    HAND_OF_THE_GODS = 'https://api.handofthegods.com/handofthegodsapi.svc'
+    PALADINS_STRIKE = 'https://api.paladinsstrike.com/paladinsstrike.svc'
 
     def switch(self, endpoint):
         if not isinstance(endpoint, self):
